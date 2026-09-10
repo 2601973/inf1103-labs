2157a098fee9a2ddb64ad24b0d62233057482c9c
Clint <2601973@sit.singaporetech.edu.sg>
2026-09-10 12:09:42 +0800

    requirement2

--- week 2/auditor.py
+++ week 2/auditor.py
@@ -1,1 +1,7 @@
 inventory = 0
+while True:
+    user_input = input("Enter stock quantity (or type 'quit' to exit):")
+
+    if user_input.lower() == "quit":
+        print("Goodbye, exiting program.")
+        break
